29cc58065ced0b7cd6f4983cd53a061e5e81a02d
Plex4000 <rajnarayanan2020@gmail.com>
2017-08-10 19:53:47 -0400

    updated my code sample list

---
+++ Implicit and Explicit Casting/Implicit and Explicit Casting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_and_Explicit_Casting
+{
+    public struct Point { public int X; public int Y; }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = 1234545543; // int is a 32-bit integer
+
+            long y = x; // Implicit conversion to 64-bit integer i.e. converting a smaller int type to a larger int type
+
+            Console.WriteLine(y);
+
+            short z = (short)x; // Explicit conversion to 16-bit integer i.e. going from a larger int type to a smaller int type. loss of infomation occurs resulting in information overflow
+
+            Console.WriteLine(z); //displays a negative number as output indicating loss of information and overflow
+        }
+    }
+}
